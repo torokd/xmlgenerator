3cfd275f0c7a869c02c785ec078d609c6f467083
Daniel Torok <torokdnl@gmail.com>
2025-10-06 17:52:10 +0200

    prettify the fillTemplate code

--- src/utils/fillTemplate.ts
+++ src/utils/fillTemplate.ts
@@ -2,10 +2,9 @@ export const fillTemplate = (template: string, values: Record<string, any>): str
   let result = template
 
   Object.entries(values).forEach(([key, value]) => {
-    // minden előfordulást lecserélünk, nem csak az elsőt
-    const regex = new RegExp(`{{\\s*${key}\\s*}}`, 'g')
-    result = result.replace(regex, String(value))
-  })
+    const regex = new RegExp(`{{\\s*${key}\\s*}}`, 'g');
+    result = result.replace(regex, String(value));
+  });
 
-  return result
+  return result;
 }
